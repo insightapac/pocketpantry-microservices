731674e0d4a733dc95ef09e8324dd7896ce6ce4d
Dawson Britto <dawson.britto@ignia.com.au>
2019-09-07 12:03:29 +0800

    Added controllers for Pantry and Recipes

---
+++ src/shopping/Common/UserDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Common
+{
+    public class UserDetails
+    {
+        public static int GetUserId()
+        {
+            //TODO: Later, get the user details from Token
+            return 1;
+        }
+    }
+}
