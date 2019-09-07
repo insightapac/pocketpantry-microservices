5c44a3df686209f573195fc8677ae7728e3cdff9
Dawson Britto <dawson.britto@ignia.com.au>
2019-09-07 17:52:44 +0800

    updates to MealPlan Model

--- src/shopping/Models/MealPlan.cs
+++ src/shopping/Models/MealPlan.cs
@@ -13,6 +13,10 @@ namespace Shopping.Models
         public int Id { get; set; }
         public int UserId { get; set; }
         public DateTime CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string ImageUri { get; set; }
+        public string Filters { get; set; }
+
 
         public User User { get; set; }
         public ICollection<MealPlanRecipe> MealPlanRecipe { get; set; }
