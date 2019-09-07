b8f369cfa38abfb3b1624f29c1c59b8d6efab4e6
Dawson Britto <dawson.britto@ignia.com.au>
2019-09-07 17:54:38 +0800

    Updated migration for MealPlan

---
+++ src/shopping/Migrations/20190907095327_MealPlanUpdate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shopping.Migrations
+{
+    public partial class MealPlanUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Filters",
+                table: "MealPlan",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUri",
+                table: "MealPlan",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MealPlan",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Filters",
+                table: "MealPlan");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUri",
+                table: "MealPlan");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MealPlan");
+        }
+    }
+}
